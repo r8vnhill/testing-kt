47f6146f2eb5fbecd255aa9de92a601478c0eaea
r8vnhill <reachme@ravenhill.cl>
2024-10-31 13:46:31 -0300

    ✨ Add PRNG module setup task and update build plugins
    - 🛠️ Introduced Gradle script to set up `PRNG` module, including creation of necessary files.
    - 🔧 Updated main build script to include `prng` plugin for consistent project configuration.

---
+++ convention-plugins/src/main/kotlin/prng.gradle.kts
@@ -0,0 +1,13 @@
+import tasks.ModuleSetupTask
+
+tasks.register<ModuleSetupTask>("setupPrngModule") {
+    description = "Creates the base module and files for the PRNG based generator lesson"
+    module.set("pbt:arbitrary:prng")
+    doLast {
+        createFiles(
+            "lists",
+            main to "average.kt",
+            test to "AverageTest.kt",
+        )
+    }
+}

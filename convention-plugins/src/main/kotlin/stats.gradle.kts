195b9409f55120f74f98cb69f521803d78794b70
r8vnhill <reachme@ravenhill.cl>
2025-03-14 20:06:51 -0300

    ✨ Add `stats` plugin and `setupStatsModule` task
    - 🏗️ Introduced `stats` plugin in `build.gradle.kts` for statistics-related functionality.
    - 🛠️ Added `setupStatsModule` task to automate statistics module creation.
    - 🔧 Fixed minor formatting issue in `gradlew` script for consistent outputs.

---
+++ convention-plugins/src/main/kotlin/stats.gradle.kts
@@ -0,0 +1,13 @@
+import tasks.ModuleSetupTask
+
+tasks.register<ModuleSetupTask>("setupStatsModule") {
+    description = "Creates the base module and files for the statistics collection lesson"
+    module.set("pbt:arbitrary:stats")
+    doLast {
+        createFiles(
+            "zoo",
+            main to "Animal.kt",
+            test to "AnimalTest.kt",
+        )
+    }
+}

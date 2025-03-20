7eb1937e540b1e97d78144c75ef6149d972e6567
r8vnhill <reachme@ravenhill.cl>
2025-03-20 18:21:42 -0300

    🛠 Removed accidental file addition & enhanced module setup
    🚨 **Fixes**
    - 🗑️ Removed unintended file `kotlin-compiler-8704326999063670670.salive`.
    
    ✨ **Enhancements**
    - 📂 Updated `arb-composition.gradle.kts` to include automatic creation of:
      - `forms/UserInput.kt`
      - `forms/FormValidationTest.kt`
      - `config/ConfigGenerators.kt`
    
    🔄 Ensured proper setup of files for the `pbt:arbitrary:composition` module.

--- convention-plugins/src/main/kotlin/arb-composition.gradle.kts
+++ convention-plugins/src/main/kotlin/arb-composition.gradle.kts
@@ -4,6 +4,14 @@ tasks.register<ModuleSetupTask>("setupArbCompositionModule") {
     description = "Creates the base module and files for the arbitrary composition lesson"
     module.set("pbt:arbitrary:composition")
     doLast {
-
+        createFiles(
+            "forms",
+            main to "UserInput.kt",
+            test to "FormValidationTest.kt",
+        )
+        createFiles(
+            "config",
+            test to "ConfigGenerators.kt"
+        )
     }
 }

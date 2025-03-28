0683f13c1f7b3c7e1dc25edbc040f28fdcdfc486
r8vnhill <reachme@ravenhill.cl>
2025-03-28 13:38:51 -0300

    ✅ Add test to verify default and custom UserConfig are not equal
    - Added a new test case to ensure that a custom configuration
      (`theme = "dark"`, `notificationsEnabled = false`) does not match
      the default configuration.
    - Introduced `shouldNotBe` assertion from Kotest.

--- matchers/predef/src/test/kotlin/cl/ravenhill/config/UserConfigTest.kt
+++ matchers/predef/src/test/kotlin/cl/ravenhill/config/UserConfigTest.kt
@@ -2,6 +2,7 @@ package cl.ravenhill.config
 
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 
 class UserConfigTest : FreeSpec({
 
@@ -9,6 +10,15 @@ class UserConfigTest : FreeSpec({
         "When using the default configuration" - {
             "Then it should match the expected default values" {
                 defaultConfig shouldBe UserConfig()
+            }
+        }
+    }
+
+    "Given a custom configuration" - {
+        "When compared to the default configuration" - {
+            "Then it should not match" {
+                val testConfig = UserConfig(theme = "dark", notificationsEnabled = false)
+                testConfig shouldNotBe defaultConfig
             }
         }
     }
